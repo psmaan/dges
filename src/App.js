53c9e3ad994b8d31bab506b841c7922b9f8c9a46
Piyush Maan <psmaan444@gmail.com>
2024-09-25 00:01:43 +0530

    content update

--- src/App.js
+++ src/App.js
@@ -14,10 +14,18 @@ import logo from './components/media/logo.png'
 import host1 from './components/media/host1.png'
 import host2 from './components/media/host2.png'
 import phone from './components/media/phone.png'
+import hotel from './components/media/hilton.jpeg'
+import tick from './components/media/tick.png'
 
 import aboutimg from './components/media/saopaulo.jpeg'
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const [activeIndex, setActiveIndex] = useState(null);
 
   const faqs = [
@@ -69,57 +77,161 @@ function App() {
   };
   return (
     <div className='w-screen overflow-hidden'>
-      <div className='h-screen w-screen landingbg'>
-        <div className='w-full flex justify-between p-4 px-8 items-start'>
-          <div className=''>
-            <img className='w-100' src={logo} />
+      <div className='min-h-screen w-screen landingbg pb-8'>
+        <div className='w-full flex justify-between p-4 sm:px-8 items-start'>
+          {/* Logo */}
+          <div>
+            <img className='w-[200px] sm:w-[300px] xl:w-[400px]' src={logo} alt='Logo' />
           </div>
-          <div className='flex items-center'>
-            <h1 className='mx-2 text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>Home</h1>
-            <a href='#about' className='decoration-none mx-2 text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>About</a>
-            <h1 className='mx-2 text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>Schedule</h1>
-            <h1 className='mx-2 text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>Speakers</h1>
-            <a href='#contact' className='decoration-none mx-2 text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>Contact</a>
-            <button className=' p-2 px-3 mx-2 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca]  duration-300 rounded-lg'>Sponsor Now</button>
+
+          {/* Hamburger Icon (visible on small screens) */}
+          <div className='lg:hidden flex items-center'>
+            <button onClick={toggleMenu} className='text-gray-700 focus:outline-none'>
+              {/* Hamburger Icon */}
+              <svg
+                className='w-8 h-8'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h16m-7 6h7'
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* Desktop Menu (hidden on small screens) */}
+          <div className='hidden lg:flex lg:items-center lg:w-auto'>
+            <div className='flex flex-row items-center'>
+              <h1 className='mx-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+                Home
+              </h1>
+              <a
+                href='#about'
+                className='decoration-none mx-2 text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+                About
+              </a>
+              <h1 className='mx-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+                Schedule
+              </h1>
+              <h1 className='mx-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+                Speakers
+              </h1>
+              <a
+                href='#contact'
+                className='decoration-none mx-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+                Contact
+              </a>
+              <button className='p-2 px-3 mx-2 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca] duration-300 rounded-lg'>
+                Sponsor Now
+              </button>
+            </div>
           </div>
         </div>
-        <div className='flex flex-col items-start pl-8 w-2/3'>
-          <h1 className='text-7xl text-gray-700 font-semibold'>Building Bridges for Industrialization</h1>
-          <h1 className='text-6xl text-gray-500 font-light'>Digital and Green Economy Synergies across the Atlantic</h1>
+
+        {/* Mobile Menu (overlay from the right) */}
+        <div
+          className={`fixed top-0 right-0 h-full w-2/3 bg-white z-50 transition-transform transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'
+            } lg:hidden`}
+        >
+          <div className='flex justify-end p-4'>
+            <button onClick={toggleMenu} className='text-gray-700 focus:outline-none'>
+              {/* Close Icon */}
+              <svg
+                className='w-8 h-8'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* Mobile Menu Links */}
+          <div className='flex flex-col items-start p-6'>
+            <h1 className='my-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+              Home
+            </h1>
+            <a
+              href='#about'
+              className='decoration-none my-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+              About
+            </a>
+            <h1 className='my-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+              Schedule
+            </h1>
+            <h1 className='my-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+              Speakers
+            </h1>
+            <a
+              href='#contact'
+              className='decoration-none my-2 text-lg xl:text-xl hover:border-b-2 border-gray-700 text-gray-700 duration-200'>
+              Contact
+            </a>
+            <button className='mt-4 p-2 px-3 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca] duration-300 rounded-lg'>
+              Sponsor Now
+            </button>
+          </div>
+        </div>
+
+        {/* Hero Section */}
+        <div className='flex flex-col items-start pl-4 sm:pl-8 sm:w-[80%] lg:w-2/3'>
+          <h1 className='mt-12 xl:mt-0 text-4xl sm:text-6xl lg:text-7xl text-gray-700 font-semibold'>
+            Building Bridges for Industrialization
+          </h1>
+          <h1 className='text-4xl w-full sm:text-5xl lg:text-6xl text-gray-500 font-light'>
+            Digital and Green Economy Synergies across the Atlantic
+          </h1>
           <div className='mt-8 flex justify-center bg-white/50 rounded-xl overflow-hidden items-center'>
-            <div className='flex items-center p-4 px-6 mr-[2px] '>
-              <img className='h-6 mr-4' src={calendar} />
-              <h1 className='text-2xl font-light'>17 November 2024</h1>
+            <div className='flex items-center p-3 sm:p-4 sm:px-6 mr-[2px]'>
+              <img className='h-4 sm:h-6 mr-1 md:mr-4' src={calendar} alt='Calendar Icon' />
+              <h1 className='text-md sm:text-xl lg:text-2xl font-light'>17 November 2024</h1>
             </div>
-            <div className='h-[40px] w-[2px] rounded-full bg-gray-700'>
-            </div>
-            <div className='flex items-center  p-4 px-6'>
-              <img className='h-6 mr-4' src={location} />
-              <h1 className='text-2xl font-light'>Sao Paulo, Brazil</h1>
+            <div className='h-[30px] sm:h-[40px] w-[2px] rounded-full bg-gray-600'></div>
+            <div className='flex items-center p-3 sm:p-4 sm:px-6'>
+              <img className='h-4 sm:h-6 mr-1 md:mr-4' src={location} alt='Location Icon' />
+              <h1 className='text-md sm:text-xl lg:text-2xl font-light'>Sao Paulo, Brazil</h1>
             </div>
           </div>
-          <div className='flex items-center mt-12'>
-            <button className=' p-2 px-3 mx-2 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca]  duration-300 rounded-lg'>Download Brochure</button>
-            <button className=' p-2 px-3 mx-2 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca]  duration-300 rounded-lg'>Buy Tickets</button>
+          <div className='flex flex-col sm:flex-row items-start sm:items-center mt-12'>
+            <button className='p-2 px-3 mx-2 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca] duration-300 rounded-lg'>
+              Download Brochure
+            </button>
+            <button className='p-2 px-3 mx-2 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca] duration-300 rounded-lg mt-4 sm:mt-0'>
+              Buy Tickets
+            </button>
           </div>
         </div>
       </div>
 
-      <div id='about' className='flex flex-col items-center w-full p-12 bg-gray-200'>
-        <h1 className='font-thin text-8xl'>DGES Overview</h1>
+      <div id='about' className='flex flex-col items-center w-full p-4 xl:p-12 bg-gray-200'>
+        <h1 className='font-thin text-4xl sm:text-6xl lg:text-8xl text-center'>DGES Overview</h1>
         <div className='flex items-start w-full mt-8'>
-          <div className='w-[50%]'>
-            <h1 className='font-light text-lg'>
+          <div className='md:w-[70%] xl:w-[50%] flex flex-col items-center md:items-start'>
+            <h1 className='font-light text-center md:text-left text-md lg:text-lg'>
               The TransAtlantic Manufacturing through Digital and Green Economy Summit is a one-day sideline event at the G20 Summit that brings together a diverse delegation of stakeholders from AfCFTA and ECOWAS nations, including The Gambia and Nigeria, in partnership with Brazil. The summit aims to build strategic bridges between Africa and South America to drive industrialization in the global south through the adoption of digital and green economy principles.
             </h1>
-            <h1 className='font-light my-4 text-lg'>
+            <div className='md:hidden w-[300px] sm:w-[400px] xl:h-[400px] xl:w-[500px] my-4'>
+              <img className='rounded-xl' src={aboutimg} />
+            </div>
+            <h1 className='font-light my-4 text-center md:text-left text-md lg:text-lg'>
               This summit will serve as a platform for high-level discussions, partnership formation, and knowledge exchange, focusing on how digital transformation and green manufacturing practices can be harnessed to accelerate industrial growth in the participating regions.
             </h1>
 
             <button className=' p-2 px-3 text-xl bg-[#9d98ea] text-white hover:bg-[#8985ca]  duration-300 rounded-lg'>Learn More</button>
 
           </div>
-          <div className='h-[400px] w-[500px] ml-20'>
+          <div className='hidden md:block w-[400px] xl:h-[400px] xl:w-[500px] ml-20'>
             <img className='rounded-xl' src={aboutimg} />
           </div>
         </div>
@@ -128,8 +240,8 @@ function App() {
       </div>
 
       <div className='w-screen flex flex-col items-center p-12 bg-[#9d98ea]'>
-        <h1 className='font-thin text-8xl text-white'>DGES Partners</h1>
-        <div className='w-full mt-12 grid grid-cols-4 gap-4'>
+        <h1 className='font-thin text-6xl lg:text-8xl text-white'>DGES Partners</h1>
+        <div className='w-full mt-12 grid grid-cols-2 md:grid-cols-4 gap-4'>
           <div className='border-gray-200/50 bg-white/10 border-2 rounded-xl h-40 flex justify-center items-center'>
             <img src={host1} />
           </div>
@@ -137,6 +249,69 @@ function App() {
             <img className='h-1/3' src={host2} />
           </div>
         </div>
+      </div>
+
+      <div className='w-full p-12 flex flex-col items-start'>
+        <h1 className='text-8xl font-thin'>The Venue</h1>
+        <div className='mt-8 w-[90%] rounded-2xl shadow-xl p-8 bg-[#9d98ea] flex items-start justify-between'>
+          <div className='flex flex-col items-start justify-between h-full'>
+            <div>
+              <h1 className='font-semibold text-6xl text-white/50'>Hilton Sao Paulo</h1>
+              <h1 className='w-[70%] font-light text-white text-xl'>Av. das Nações Unidas, 12901 - Brooklin, São Paulo - SP, 04578-000, Brazil</h1>
+            </div>
+
+            <button className='mt-36 p-2 px-4 text-[#9d98ea] bg-white rounded-lg font-bold'>Get Directions</button>
+          </div>
+          <div className='w-[400px] '>
+            <img className='rounded-xl shadow-xl' src={hotel} />
+          </div>
+        </div>
+
+      </div>
+
+      <div className='w-full p-12 flex flex-col items-start bg-[#9d98ea]'>
+        <h1 className='text-8xl font-thin text-white'>Get the tickets</h1>
+        <div className='mt-8 w-[90%] rounded-2xl shadow-xl p-8 bg-white flex items-start justify-between'>
+          <div className='flex flex-col items-start justify-between h-full w-[60%]'>
+            <div className='flex justify-between w-full'>
+              <h1 className='font-light text-4xl text-[#9d98ea]'>Premium Ticket</h1>
+              <h1 className=' ml-auto font-semibold text-gray-700 text-6xl'>$200</h1>
+            </div>
+            <div className='flex items-center'>
+              <div className='h-6 w-6 flex justify-center items-center bg-[#9d98ea] rounded-full'>
+                <img className='w-[70%]' src={tick} />
+
+              </div>
+              <h1 className='font-light text-xl ml-4'>Ticket Benefit 1</h1>
+            </div>
+            <div className='flex items-center'>
+              <div className='h-6 w-6 flex justify-center items-center bg-[#9d98ea] rounded-full'>
+                <img className='w-[70%]' src={tick} />
+
+              </div>
+              <h1 className='font-light text-xl ml-4 mt-2'>Ticket Benefit 1</h1>
+            </div>
+            <div className='flex items-center'>
+              <div className='h-6 w-6 flex justify-center items-center bg-[#9d98ea] rounded-full'>
+                <img className='w-[70%]' src={tick} />
+
+              </div>
+              <h1 className='font-light text-xl ml-4 mt-2'>Ticket Benefit 1</h1>
+            </div>
+            <div className='flex items-center'>
+              <div className='h-6 w-6 flex justify-center items-center bg-[#9d98ea] rounded-full'>
+                <img className='w-[70%]' src={tick} />
+
+              </div>
+              <h1 className='font-light text-xl ml-4 mt-2'>Ticket Benefit 1</h1>
+            </div>
+            <button className='mt-20 p-2 px-4 bg-[#9d98ea] text-white rounded-lg font-bold'>Buy Now</button>
+          </div>
+          <div className='w-[400px] '>
+            <img className='rounded-xl shadow-xl' src={hotel} />
+          </div>
+        </div>
+
       </div>
 
       <div className="w-screen bg-gray-200 flex flex-col items-center py-12 px-4">
@@ -166,21 +341,19 @@ function App() {
       </div>
       <div id='contact' className='w-screen flex justify-between p-8 px-12 bg-gray-800'>
         <div className='flex flex-col items-start'>
-          <h1 className='text-6xl font-semibold text-gray-200'>Get in Touch</h1>
+          <h1 className='text-4xl sm:text-6xl font-semibold text-gray-200'>Get in Touch</h1>
           <div className='flex items-center mt-4'>
             <img className='h-7' src={phone} />
-            <h1 className='ml-4 text-2xl font-light text-gray-200'>+234 (703) 565-2264</h1>
+            <h1 className='ml-4 text-lg sm:text-2xl font-light text-gray-200'>+234 (703) 565-2264</h1>
 
           </div>
           <div className='flex items-center mt-2'>
             <img className='h-7' src={phone} />
-            <h1 className='ml-4 text-2xl font-light text-gray-200'>+1 (617) 602-6272</h1>
-
+            <h1 className='ml-4 text-lg sm:text-2xl font-light text-gray-200'>+1 (617) 602-6272</h1>
           </div>
-
         </div>
-        <div className='p-2 bg-white/20 rounded-xl'>
-          <img src={logo} />
+        <div className='hidden sm:block p-2 bg-white/20 rounded-xl'>
+          <img className='w-[300px] sm:w-[400px] lg:w-[500px]' src={logo} />
         </div>
       </div>
     </div>
